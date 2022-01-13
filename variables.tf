e14efcf9b612a56f0105ca176f75d1c2927d51bc
Nareshpv01 <97612132+Nareshpv01@users.noreply.github.com>
2022-01-13 12:22:56 +0530

    Create  variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "naresh"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
